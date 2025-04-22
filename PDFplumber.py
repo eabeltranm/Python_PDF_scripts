6d7813f2cb95ca9c0ca9338521c4cc3a1e5e7e84
eabeltranm <eabeltranm@gmail.com>
2025-04-22 14:59:35 -0500

    Modify arguments in the extract_tables function

--- PDFplumber.py
+++ PDFplumber.py
@@ -3,11 +3,18 @@ import pandas as pd
 
 with pdfplumber.open("C:\\Users\\eabeltranm\\Documents\\Amor\\Nuevo_proceso\\invima_1.pdf") as pdf:
     first_page = pdf.pages[0]
-    tables = first_page.extract_tables({'vertical_strategy':'lines',
-                                        'horizontal_strategy':'lines',
-                                        'intersection_x_tolerance':10,
-                                        'intersection_y_tolerance':10
-                                        })
-    for table in tables:
+    tables_on_page = first_page.extract_tables({
+        'vertical_strategy': 'lines',
+        'horizontal_strategy': 'lines',
+        'intersection_x_tolerance': 5,
+        'intersection_y_tolerance': 5,
+        'snap_tolerance': 3,
+        'join_tolerance': 2,
+        'edge_min_length': 5,
+        'min_words_vertical': 2,
+        'min_words_horizontal': 2,
+        'keep_blank_chars': True
+    })
+    for table in tables_on_page:
         df = pd.DataFrame(table)
         print(df)
